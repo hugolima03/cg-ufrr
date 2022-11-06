f0ce0c26dcefe3c7668fabefe4754acda9510e13
Hugo Lima Romão <hugo8romao@gmail.com>
2022-11-06 17:17:46 -0400

    Adding second octant support to bresenham algorithm

--- src/functions/lineRasterization/bresenham.js
+++ src/functions/lineRasterization/bresenham.js
@@ -63,10 +63,24 @@
 
 export function bresenham(p1, p2) {
   console.time("Método de Bresenham");
-  const path = []; // Lista com as coordenadas dos pixels
+  let path = []; // Lista com as coordenadas dos pixels
 
-  const dx = p2.x - p1.x;
-  const dy = p2.y - p1.y;
+  let x1 = p1.x;
+  let y1 = p1.y;
+  let x2 = p2.x;
+  let y2 = p2.y;
+
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+
+  const secondoOctante = dy > dx;
+
+  if (secondoOctante) {
+    [x1, y1] = [y1, x1];
+    [x2, y2] = [y2, x2];
+  }
+  dx = x2 - x1;
+  dy = y2 - y1;
 
   let d = 2 * dy - dx; // Valor inicial de d
 
@@ -77,7 +91,7 @@ export function bresenham(p1, p2) {
 
   path.push({ x, y });
 
-  while (x < p2.x) {
+  while (x < x2) {
     if (d <= 0) {
       d += incE; // Escolhe E
       x++;
@@ -87,6 +101,10 @@ export function bresenham(p1, p2) {
       y++;
     }
     path.push({ x, y });
+  }
+
+  if (secondoOctante) {
+    path = path.map(({ x, y }) => ({ x: y, y: x }));
   }
 
   console.timeEnd("Método de Bresenham");
