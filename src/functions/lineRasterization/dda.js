b40e6591e1d22f9e2b7e16c6b4972f700aa6e09c
Hugo Lima Romão <hugo8romao@gmail.com>
2022-11-09 18:54:52 -0400

    removing unused dda algorithm

--- src/functions/lineRasterization/dda.js
+++ src/functions/lineRasterization/dda.js
@@ -1,6 +1,5 @@
 export function dda(p1, p2) {
   console.time("Método dda");
-
   const path = [];
 
   // Calcula as variações de X e Y
@@ -26,21 +25,3 @@ export function dda(p1, p2) {
   console.timeEnd("Método dda");
   return path;
 }
-
-// export function dda(p1, p2) {
-//   console.time("dda algorithm");
-
-//   const path = [];
-
-//   const dx = p2.x - p1.x;
-//   const dy = p2.y - p1.y;
-
-//   const m = dy / dx; // coefficient of the line.
-//   const b = p1.y - m * p1.x; // linear coefficient of the line.
-
-//   for (let i = 0; i <= dx; i++) {
-//     path.push({ x: i, y: Math.round(m * (p1.x + i) + b) });
-//   }
-//   console.timeEnd("dda algorithm");
-//   return path;
-// }
