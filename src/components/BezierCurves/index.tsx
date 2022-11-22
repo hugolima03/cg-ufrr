faa31e437faae20b8d96319ec4478e68c052991b
Hugo Lima Romão <hugo8romao@gmail.com>
2022-11-22 18:23:56 -0400

    Parametric equation for bezier curves

--- src/components/BezierCurves/index.tsx
+++ src/components/BezierCurves/index.tsx
@@ -1,44 +1,33 @@
+import { Form } from "components/FillAlgorithmsForm/styles";
+import { Span } from "components/LineRasterizationForm/styles";
 import { Pixel } from "contexts/PixelGridContext";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 import { Column, Wrapper } from "templates/Base/styles";
 
 const PLOT_HEIGHT = 500;
 
-// class Graph {
-//   constructor(id) {
-//     this.el = document.getElementById(id);
-//   }
-
-//   drawLine(point1, point2, stroke = 2, color = "#000000") {
-//     this.el.insertAdjacentHTML(
-//       "beforeend",
-//       `<line x1="${point1.x}" y1="${point1.y}" x2="${point2.x}" y2="${point2.y}" stroke="${color}" stroke-width="${stroke}" id="line"/>`
-//     );
-//   }
-
-//   drawCurveFromPoints(points) {
-//     for (let i = 0; i < points.length; i++) {
-//       if (i + 1 < points.length) this.drawLine(points[i], points[i + 1]);
-//     }
-//   }
-// }
-
 const BezierCurves = () => {
-  const numDrawingPoints = 100;
-  const drawingPoints = [];
+  let drawingPoints: Pixel[] = [];
+  const [debugPoints, setDebugPoints] = useState<Pixel[]>([]);
+
+  const [controlPoints, setControlPoints] = useState<Pixel[]>([
+    { x: 10, y: 10 },
+  ]);
 
   const [lines, setLines] = useState<React.ReactNode[]>([]);
 
   function drawLine(p1: Pixel, p2: Pixel, stroke = 2, color = "#000000") {
     const newLine = (
       <line
+        key={(p1.x, p1.y, p2.x, p2.y, Math.random())}
         x1={p1.x}
         y1={p1.y}
         x2={p2.x}
         y2={p2.y}
         stroke={color}
-        stroke-width={stroke}
+        strokeWidth={stroke}
       />
     );
     setLines((lines) => [...lines, newLine]);
@@ -46,26 +35,28 @@ const BezierCurves = () => {
 
   function drawCurveFromPoints(points: Pixel[]) {
     for (let i = 0; i < points.length; i++) {
-      if (i + 1 < points.length) drawLine(points[i], points[i + 1]);
+      if (i + 1 < points.length) {
+        drawLine(points[i], points[i + 1]);
+      }
     }
   }
 
-  function calculateNewPoint(t) {
+  function calculateNewPoint(t: number) {
     // Coordinates calculated using the general formula are relative to
     // origin at bottom left.
     let x = 0;
     let y = 0;
-    let n = points.length - 1;
+    let n = controlPoints.length - 1;
     for (let i = 0; i <= n; i++) {
       let bin = C(n, i) * Math.pow(1 - t, n - i) * Math.pow(t, i);
-      x += bin * points[i].x;
-      y += bin * points[i].y;
+      x += bin * controlPoints[i].x;
+      y += bin * controlPoints[i].y;
     }
 
-    return new Point(x, PLOT_HEIGHT - y);
+    return { x: x, y: PLOT_HEIGHT - y };
   }
 
-  function calculateDrawingPoints() {
+  function calculateDrawingPoints(numDrawingPoints: number) {
     let interval = 1 / numDrawingPoints;
     let t = interval;
 
@@ -77,22 +68,141 @@ const BezierCurves = () => {
     }
   }
 
-  useEffect(() => {
-    drawCurveFromPoints([
-      { x: 1, y: 1 },
-      { x: 200, y: 50 },
-    ]);
-  }, []);
+  function drawHandles() {
+    if (controlPoints.length === 1) {
+      setDebugPoints([controlPoints[0]]);
+      return;
+    }
+    for (let i = 1; i < controlPoints.length; i++) {
+      if (i == 1 || i == controlPoints.length - 1) {
+        setDebugPoints((points) => [
+          ...points,
+          controlPoints[i - 1],
+          controlPoints[i],
+        ]);
+      }
+
+      const temp = controlPoints.map(({ x, y }) => ({ x, y: PLOT_HEIGHT - y }));
+
+      drawLine(
+        temp[i - 1],
+        temp[i],
+        1,
+        i == 1 || i == temp.length - 1 ? "#00FF00" : "#AA4444"
+      );
+    }
+
+    if (controlPoints.length === 1) {
+      setDebugPoints([controlPoints[0]]);
+      return;
+    }
+  }
+
+  function C(n: number, k: number) {
+    var coeff = 1;
+    for (var x = n - k + 1; x <= n; x++) coeff *= x;
+    for (x = 1; x <= k; x++) coeff /= x;
+    return coeff;
+  }
+
+  function onSubmit(data: any) {
+    setLines([]);
+    setDebugPoints([]);
+
+    drawingPoints = [];
+    calculateDrawingPoints(Number(data.numDrawingPoints));
+    drawHandles();
+    drawCurveFromPoints(drawingPoints);
+  }
+
+  const { handleSubmit, register, watch } = useForm({
+    defaultValues: {
+      algorithm: "parametric",
+    },
+  });
+
+  function onInputChange(newValue: number, index: number, axis: "x" | "y") {
+    setControlPoints((points) => {
+      const temp = [...points];
+
+      if (axis === "x") {
+        temp[index] = { ...temp[index], x: newValue };
+      } else {
+        temp[index] = { ...temp[index], y: newValue };
+      }
+
+      return [...temp];
+    });
+  }
 
   return (
     <Wrapper>
-      <Column style={{ backgroundColor: "gray" }}>
-        bezierCurves
+      <Column>
+        <Form>
+          <select
+            {...register("algorithm", { required: "obrigatório" })}
+            autoFocus
+          >
+            <option value="parametric">Equação paramétrica</option>
+            <option value="casteljau">Casteljau</option>
+          </select>
+          {watch("algorithm") === "parametric" && (
+            <>
+              <label>pontos</label>
+              {controlPoints.map(({ x, y }, index) => (
+                <Span key={x + y + index}>
+                  <input
+                    type="number"
+                    defaultValue={x}
+                    onChange={(e) =>
+                      onInputChange(Number(e.target.value), index, "x")
+                    }
+                    step={100}
+                  />
+                  <input
+                    type="number"
+                    defaultValue={y}
+                    onChange={(e) =>
+                      onInputChange(Number(e.target.value), index, "y")
+                    }
+                    step={100}
+                  />
+                </Span>
+              ))}
+
+              <label>numDrawingPoints</label>
+
+              <input
+                type="range"
+                min={1}
+                max={101}
+                {...register("numDrawingPoints" as any)}
+                defaultValue={50}
+                step={5}
+              />
+
+              <button
+                type="button"
+                onClick={() =>
+                  setControlPoints((points) => [...points, { x: 0, y: 0 }])
+                }
+              >
+                Adicionar ponto de controle
+              </button>
+            </>
+          )}
+
+          <input type="submit" onClick={handleSubmit(onSubmit)} />
+        </Form>
+      </Column>
+      <Column style={{ backgroundColor: "white" }}>
         <svg width="500px" height="500px" id="graph">
           {lines.map((line) => line)}
+          {debugPoints.map(({ x, y }) => (
+            <circle key={x + y} cx={x} cy={PLOT_HEIGHT - y} r="5" fill="#000" />
+          ))}
         </svg>
       </Column>
-      <Column>bezierCurves</Column>
     </Wrapper>
   );
 };
